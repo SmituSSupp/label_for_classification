0c3948eb5c4c5b2b125de76e29add16662d61834
SmituSSupp <mixercool1@gmail.com>
2020-01-02 22:12:38 +0300

    completed main part, TODO creating json after finishing all preparations

--- github_alb.py
+++ github_alb.py
@@ -2,13 +2,17 @@ from PIL import Image, ImageTk
 import tkinter
 import os
 
+
 base_path = 'NY_corp_test'
 suitable_path = 'suitable_dir'#left
 unsuitable_path = 'unsuitable_dir'#right
 image_list = os.listdir('NY_corp_test')
+label_list = image_list.copy()
 current = 0
 photo_num = 0
 
+
+'''
 def move(Rd):
     global current, image_list,photo_num
     if not (0 <= current + Rd < len(image_list)):
@@ -46,16 +50,23 @@ def rightKey(event):
     image = Image.open(os.path.join(base_path,image_list[current]))
     image.save(unsuitable_path+'/'+image_list[current])
     print ("Right key pressed")
-    
+'''    
+
 def main():
     def move(Rd):
         global current, image_list,photo_num
-        if not (0 <= current + Rd < len(image_list)):
-            return
+        #if not (0 <= current + Rd < len(image_list)):
+        #    return
         current += Rd
         photo_num += Rd
+        if (photo_num>=len(image_list)):
+            label['text'] = 'This was we last photo'
+            return
         image = Image.open(os.path.join(base_path,image_list[current]))
         photo = ImageTk.PhotoImage(image.resize((720,640)))
+        label['text'] = label_list[current]
+        label['image'] = photo
+        label.photo = photo
 
     def next_img(event):
         global current, image_list,photo_num
@@ -71,25 +82,33 @@ def main():
         
         image = Image.open(os.path.join(base_path,image_list[current]))
         photo = ImageTk.PhotoImage(image.resize((720,640)))
-        label['text'] = str(photo_num)
+        label['text'] = label_list[current]
         label['image'] = photo
         label.photo = photo
 
     def leftKey(event):
         global current, image_list,photo_num
         image = Image.open(os.path.join(base_path,image_list[current]))
+        if (os.path.exists(unsuitable_path+'/'+image_list[current])):
+            print('wow changed opinion')
+            os.remove(unsuitable_path+'/'+image_list[current])
+        label_list[current] = image_list[current]+' class suitable photo'
         image.save(suitable_path+'/'+image_list[current])
         print ("Left key pressed")
 
     def rightKey(event):
         global current, image_list,photo_num
         image = Image.open(os.path.join(base_path,image_list[current]))
+        if (os.path.exists(suitable_path+'/'+image_list[current])):
+            print('wow changed opinion')
+            os.remove(suitable_path+'/'+image_list[current])
+        label_list[current] = image_list[current]+' class unsuitable photo'
         image.save(unsuitable_path+'/'+image_list[current])
         print ("Right key pressed")
 #Importing Photos
 
-##    base_path = 'NY_corp_test'
-##    suitable_path = 'suitable_dir'#left
+#    base_path = 'NY_corp_test'
+#    suitable_path = 'suitable_dir'#left
 #    unsuitable_path = 'unsuitable_dir'#right
 
     if (not os.path.exists(suitable_path)):
@@ -98,14 +117,17 @@ def main():
         os.mkdir(unsuitable_path)
 
 #    image_list = os.listdir('NY_corp_test')
-
- #   current = 0
- #   photo_num = 0
+#   current = 0
+#   photo_num = 0
 
     root = tkinter.Tk()
     label = tkinter.Label(root, compound=tkinter.TOP)
     label.pack()
-
+    image = Image.open('placeholder.jpg')
+    photo = ImageTk.PhotoImage(image)
+    label['text'] = 'tutorial'
+    label['image'] = photo
+    label.photo = photo
     frame = tkinter.Frame(root)
     frame.pack()
 
